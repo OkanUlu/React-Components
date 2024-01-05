48c5a5c47647bda38c133d9507ec012a1a9716ba
Okan Ulu <okanulu048@gmail.com>
2024-01-05 15:23:15 +0300

    Grid Dashboard Added

--- Data Visualizations/src/App.js
+++ Data Visualizations/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 import LineChart from './components/Line';
 import AreaChart from './components/Area';
 import StackedLineChart from './components/StackedLine';
@@ -9,19 +10,45 @@ import StackedBarChart from './components/StackedBar';
 import PieChart from './components/Pie';
 import StackedPieChart from './components/StackedPie';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <LineChart />
-      <AreaChart />
-      <StackedLineChart />
-      <StackedAreaChart />
-      <BarChart />
-      <StackedBarChart />
-      <PieChart />
-      <StackedPieChart />
+      <div className="chart-grid">
+        <div className="chart-card">
+          <h2>Line Chart</h2>
+          <LineChart />
+        </div>
+        <div className="chart-card">
+          <h2>Area Chart</h2>
+          <AreaChart />
+        </div>
+        <div className="chart-card">
+          <h2>Stacked Line Chart</h2>
+          <StackedLineChart />
+        </div>
+        <div className="chart-card">
+          <h2>Stacked Area Chart</h2>
+          <StackedAreaChart />
+        </div>
+        <div className="chart-card">
+          <h2>Bar Chart</h2>
+          <BarChart />
+        </div>
+        <div className="chart-card">
+          <h2>Stacked Bar Chart</h2>
+          <StackedBarChart />
+        </div>
+        <div className="chart-card">
+          <h2>Pie Chart</h2>
+          <PieChart />
+        </div>
+        <div className="chart-card">
+          <h2>Stacked Pie Chart</h2>
+          <StackedPieChart />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
